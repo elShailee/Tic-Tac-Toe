5d91f53d44bda631f5ca7528d9fa86e2babe092a
shailee <shailee.eliyahu@gmail.com>
2021-10-07 15:50:08 +0300

    restored broken GameplayScreen

--- front/src/Screens/GameplayScreen/GameplayScreen.jsx
+++ front/src/Screens/GameplayScreen/GameplayScreen.jsx
@@ -4,36 +4,31 @@ import { DataUtilsContainer, GameContainer } from './styles';
 import apiCallsHandler from 'Utils/axiosFuncs';
 
 export default function GameplayScreen({ gameState, setGameState }) {
-	// const resetGameState = async () => {
-	// 	const newGameState = await apiCallsHandler.putGame({
-	// 		...gameState,
-	// 		...blankGameState,
-	// 		turnState: gameState.startingPlayer,
-	// 	});
-	// 	newGameState && setGameState(newGameState);
-	// };
-	return 'gameplay';
+	const resetGameState = async () => {
+		const newGameState = 'need a call';
+		newGameState && setGameState(newGameState);
+	};
 
-	// return (
-	// <GameContainer>
-	// 	<GameGrid gameState={gameState} setGameState={setGameState} />
-	// 	<DataUtilsContainer>
-	// 		{'Player Turn: ' + gameState.turnState}
-	// 		<br />
-	// 		<br />
+	return (
+		<GameContainer>
+			<GameGrid gameState={gameState} setGameState={setGameState} />
+			<DataUtilsContainer>
+				{'Player Turn: ' + gameState.turnState}
+				<br />
+				<br />
 
-	// 		{'Win State: ' + (gameState.winState || 'awaiting results...')}
-	// 		<br />
-	// 		<br />
-	// 		<button onClick={resetGameState}>Reset Board State</button>
-	// 		<br />
+				{'Win State: ' + (gameState.winState || 'awaiting results...')}
+				<br />
+				<br />
+				<button onClick={resetGameState}>Reset Board State</button>
+				<br />
 
-	// 		{'Game ID: '}
-	// 		<input type='text' defaultValue={gameState.gameId} readOnly disabled />
-	// 		<br />
+				{'Game ID: '}
+				<input type='text' defaultValue={gameState.gameId} readOnly disabled />
+				<br />
 
-	// 		<button onClick={() => setGameState(blankGameState)}>{'Save&Exit'}</button>
-	// 	</DataUtilsContainer>
-	// </GameContainer>
-	// );
+				<button onClick={() => setGameState({})}>{'Save&Exit'}</button>
+			</DataUtilsContainer>
+		</GameContainer>
+	);
 }
