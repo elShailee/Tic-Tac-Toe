ffe381182e67868eadf3d37af9a74c8baa0e7f96
shailee <shailee.eliyahu@gmail.com>
2021-10-19 11:56:50 +0300

    working particles implementation

--- front/src/App/App.js
+++ front/src/App/App.js
@@ -1,11 +1,21 @@
 // import { useState } from 'react';
-import { AppContainer, Left, Game, Right, GameContainer } from './styles';
+import {
+	AppContainer,
+	LeftContainerForOverflow,
+	LeftParticles,
+	Game,
+	RightContainerForOverflow,
+	RightParticles,
+	GameContainer,
+} from './styles';
 // import { enviroment } from 'envSelector';
 // import DevelopemetToolbar from './DemelopementToolbar';
 // import GameplayScreen from 'Screens/GameplayScreen/GameplayScreen';
 // import HomeScreen from 'Screens/HomeScreen/HomeScreen';
 import { ThemeProvider } from 'styled-components';
 import theme from 'theme';
+import leftSvg from 'Assets/left.svg';
+import rightSvg from 'Assets/right.svg';
 
 function App() {
 	// const [gameState, setGameState] = useState({});
@@ -13,11 +23,16 @@ function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<AppContainer>
-				<Left></Left>
+				<LeftContainerForOverflow>
+					<LeftParticles src={leftSvg} />
+				</LeftContainerForOverflow>
 				<GameContainer>
 					<Game></Game>
 				</GameContainer>
-				<Right></Right>
+				<RightContainerForOverflow>
+					<RightParticles src={rightSvg} />
+				</RightContainerForOverflow>
+
 				{/* {enviroment === 'developement' && <DevelopemetToolbar gameState={gameState} setGameState={setGameState} />}
 				{gameState.gameId ? (
 					<GameplayScreen gameState={gameState} setGameState={setGameState} />
--- front/src/App/styles.js
+++ front/src/App/styles.js
@@ -6,8 +6,15 @@ export const AppContainer = styled.div`
 	grid-template-columns: 1fr auto 1fr;
 `;
 
-export const Left = styled.div`
-	background-color: blue;
+export const LeftContainerForOverflow = styled.div`
+	direction: rtl;
+	overflow: hidden;
+	height: 100vh;
+`;
+
+export const LeftParticles = styled.img`
+	height: 100vh;
+	overflow: hidden;
 `;
 
 export const Game = styled.div`
@@ -24,11 +31,17 @@ export const GameContainer = styled.div`
 	width: 100vh;
 	height: 100vh;
 	max-width: 100vw;
-	background-color: pink;
+	/* background-color: pink; */
 	display: flex;
 	align-items: center;
 `;
 
-export const Right = styled.div`
-	background-color: blue;
+export const RightContainerForOverflow = styled.div`
+	overflow: hidden;
+	height: 100vh;
+`;
+
+export const RightParticles = styled.img`
+	height: 100vh;
+	overflow: hidden;
 `;
