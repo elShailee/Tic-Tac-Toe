38bebef20b7bde0e9f814d0c5e10a426eeea27d1
shailee <shailee.eliyahu@gmail.com>
2021-10-25 14:41:09 +0300

    implemented dark bg particles
    need to add shadows and themes infrastructure

--- front/src/App/App.js
+++ front/src/App/App.js
@@ -1,21 +1,11 @@
 // import { useState } from 'react';
-import {
-	AppContainer,
-	LeftContainerForOverflow,
-	LeftParticles,
-	Game,
-	RightContainerForOverflow,
-	RightParticles,
-	GameContainer,
-} from './styles';
+import { AppContainer, Game, GameContainer } from './styles';
 // import { enviroment } from 'envSelector';
 // import DevelopemetToolbar from './DemelopementToolbar';
 // import GameplayScreen from 'Screens/GameplayScreen/GameplayScreen';
 // import HomeScreen from 'Screens/HomeScreen/HomeScreen';
 import { ThemeProvider } from 'styled-components';
 import theme from 'theme';
-import leftSvg from 'Assets/left.svg';
-import rightSvg from 'Assets/right.svg';
 
 function App() {
 	// const [gameState, setGameState] = useState({});
@@ -23,15 +13,9 @@ function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<AppContainer>
-				<LeftContainerForOverflow>
-					<LeftParticles src={leftSvg} />
-				</LeftContainerForOverflow>
 				<GameContainer>
 					<Game></Game>
 				</GameContainer>
-				<RightContainerForOverflow>
-					<RightParticles src={rightSvg} />
-				</RightContainerForOverflow>
 
 				{/* {enviroment === 'developement' && <DevelopemetToolbar gameState={gameState} setGameState={setGameState} />}
 				{gameState.gameId ? (
--- front/src/App/styles.js
+++ front/src/App/styles.js
@@ -1,20 +1,10 @@
 import styled from 'styled-components';
+import darkBG from 'Assets/dark_bg.svg';
 
 export const AppContainer = styled.div`
 	width: 100vw;
-	display: grid;
-	grid-template-columns: 1fr auto 1fr;
-`;
-
-export const LeftContainerForOverflow = styled.div`
-	direction: rtl;
-	overflow: hidden;
-	height: 100vh;
-`;
-
-export const LeftParticles = styled.img`
-	height: 100vh;
-	overflow: hidden;
+	${({ theme }) => theme.customStyles.centerItems}
+	background: url(${darkBG});
 `;
 
 export const Game = styled.div`
@@ -31,17 +21,6 @@ export const GameContainer = styled.div`
 	width: 100vh;
 	height: 100vh;
 	max-width: 100vw;
-	/* background-color: pink; */
 	display: flex;
 	align-items: center;
-`;
-
-export const RightContainerForOverflow = styled.div`
-	overflow: hidden;
-	height: 100vh;
-`;
-
-export const RightParticles = styled.img`
-	height: 100vh;
-	overflow: hidden;
 `;
