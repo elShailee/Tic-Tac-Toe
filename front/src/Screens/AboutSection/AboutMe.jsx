b872d30569d1661c54f58bed3aa392c12b08008a
shailee <shailee.eliyahu@gmail.com>
2021-11-26 11:30:22 +0200

    CV and Icons Updates

--- front/src/Screens/AboutSection/AboutMe.jsx
+++ front/src/Screens/AboutSection/AboutMe.jsx
@@ -7,12 +7,12 @@ export default function AboutMe() {
 			<NameUnderline />
 			<NameContainer />
 			<ParagraphContainer>
-				<TextLine margin={29}> I'm a 25 y/o developer from Bet-Shemesh.</TextLine>
-				<TextLine margin={27}> I enjoy music and art, and loved being a Youth</TextLine>
-				<TextLine margin={22}> Guide (informal education) for five years.</TextLine>
+				<TextLine margin={29}> I'm a 25 y/o developer from Beit-Shemesh.</TextLine>
+				<TextLine margin={27}> I enjoy music and art, and loved being a youth</TextLine>
+				<TextLine margin={22}> guide (informal education) for five years.</TextLine>
 				<TextLine>For the last two years I am studying CS at the Open University</TextLine>
 				<TextLine>of Israel, and learning to program.</TextLine>
-				<TextLine>Learning alone and with friend was alot of fun, but now I'm</TextLine>
+				<TextLine>Learning alone and with friends was alot of fun, but now I'm</TextLine>
 				<TextLine>looking forward to work with good people on a great product!</TextLine>
 			</ParagraphContainer>
 			<ProfileImage />
